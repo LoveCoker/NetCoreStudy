90294d0f3aebfe573ed4af9363b0ab92886546dc
QingLe <1131624815@qq.com>
2017-09-13 11:25:04 +0800

    增加一个测试方法

---
+++ NetCoreUseSQL/Models/NewTestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreUseSQL.Models
+{
+    public class NewTestClass
+    {
+        public void Test()
+        {
+            Console.WriteLine("这是一个测试方法");
+        }
+    }
+}
